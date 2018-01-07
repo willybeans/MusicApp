4b3091c9cf031255a82eafb1daec7c4f891b1d37
willybeans <willwedmedyk@gmail.com>
2018-01-07 00:24:15 -0500

    adding styling

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -5,12 +5,8 @@ class ContactItem extends Component {
     this.props.onDelete(id);
   }
   render() {
-    let botBorder = {
-      'border-bottom' : '5px solid',
-      'list-style-type' : 'none'
-     };
     return (
-      <div className="Contacts_List" style={botBorder}>
+      <div className="contactItems shadow">
         <h4> {this.props.contact.title}
         <a href="#" onClick={this.deleteContact.bind(this, this.props.contact.id)}> X </a>
         </h4>
